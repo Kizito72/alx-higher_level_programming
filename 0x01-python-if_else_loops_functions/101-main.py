619b6909824f972b63847fec2a87d6de93232828
kizito72 <kizitorawbiz@gmail.com>
2023-11-29 19:54:54 -0800

    Identation errors and other errors fixed

--- 0x01-python-if_else_loops_functions/101-main.py
+++ 0x01-python-if_else_loops_functions/101-main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-remove_char_at = __import__('101-remove_char_at').remove_char_at
+
+def remove_char_at(s, n):
+    if n < 0 or n >= len(s):
+        return s
+    return s[:n] + s[n+1:]
 
 print(remove_char_at("Best School", 3))
 print(remove_char_at("Chicago", 2))
 print(remove_char_at("C is fun!", 0))
 print(remove_char_at("School", 10))
 print(remove_char_at("Python", -2))
-
